5b231ddf8bff42a2295fb9d42deda65084042f39
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-10 13:12:16 +0900

    feature: set fastlane in project file
    feature: set fastlane in project file

---
+++ SickSangHaeUITests/SickSangHaeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SickSangHaeUITestsLaunchTests.swift
+//  SickSangHaeUITests
+//
+//  Created by CHANG JIN LEE on 2023/07/10.
+//
+
+import XCTest
+
+final class SickSangHaeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
