6e82237abd1d89e013d544709b2825468f9384cf
Reene <vernum.amor@gmail.com>
2023-07-10 18:03:45 +0900

    feature: Update AddItemView
    feature: Update AddItemView

--- SickSangHae/View/AddItemView.swift
+++ SickSangHae/View/AddItemView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct AddItemView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                Text("직접 추가")
+                    .fontWeight(.bold)
+                    .font(.title2)
+                
+                Image(systemName: "xmark")
+                    .frame(width: 15.5, height: 15.5)
+                    .padding(.leading, 123)
+            }
+            .padding(.trailing, 22)
+            
+            HStack {
+                Image(systemName: "chevron.left")
+                    .frame(width: 8, height: 14.2)
+                
+                Text("2024년 7월 5일")
+                    .fontWeight(.bold)
+                    .padding(34)
+                
+                Image(systemName: "chevron.right")
+                    .frame(width: 8, height: 14.2)
+                    
+                    
+            }
+        }
     }
 }
 
