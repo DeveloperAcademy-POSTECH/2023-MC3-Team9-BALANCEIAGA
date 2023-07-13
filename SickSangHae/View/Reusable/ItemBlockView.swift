749a4671bebdca024a98cb01f2f336cc16a43614
Reene <vernum.amor@gmail.com>
2023-07-13 17:22:34 +0900

    Revert "feature: add ItemBlockView.swift"
    This reverts commit a8a74aec6af3bd5f5be9daae81e4245177bd9c9d.

--- SickSangHae/View/Reusable/ItemBlockView.swift
+++ SickSangHae/View/Reusable/ItemBlockView.swift
@@ -9,31 +9,7 @@ import SwiftUI
 
 struct ItemBlockView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-                .frame(width: 350, height: 116)
-                .foregroundColor(.tempGrayColor)
-                .cornerRadius(12)
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("품목")
-                        .padding(.leading,20)
-                    Text("품목을 입력해주세요.")
-                        .foregroundColor(.lightGrayColor)
-                        .padding(.leading,30)
-                }
-                Divider()
-                    .frame(width: 350, height: 10)
-                    .foregroundColor(.whatGrayColor)
-                HStack {
-                    Text("금액")
-                        .padding(.leading,20)
-                    Text("금액을 입력해주세요.")
-                        .foregroundColor(.lightGrayColor)
-                        .padding(.leading,30)
-                }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
