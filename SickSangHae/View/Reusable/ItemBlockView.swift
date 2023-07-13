7b4c96bf73d87637cc35e21965af8dfbc917cd31
Reene <vernum.amor@gmail.com>
2023-07-13 17:15:22 +0900

    feature: add ItemBlockView.swift
    feature: add ItemBlockView.swift

---
+++ SickSangHae/View/Reusable/ItemBlockView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemBlockView.swift
+//  SickSangHae
+//
+//  Created by Narin Kang on 2023/07/13.
+//
+
+import SwiftUI
+
+struct ItemBlockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemBlockView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemBlockView()
+    }
+}
