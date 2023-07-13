6ff06e18318a007608ccb44d261c4993c2b8cc5f
Reene <vernum.amor@gmail.com>
2023-07-13 15:52:50 +0900

    feature: add UpperTabBar.swift
    feature: add UpperTabBar.swift
    create button preview in mainView

--- SickSangHae/View/Reusable/UpperTabBar.swift
+++ SickSangHae/View/Reusable/UpperTabBar.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct UpperTabBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.06)
+                .background(
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0, green: 0.83, blue: 0.65), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0, green: 0.58, blue: 1), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.09, y: 0),
+                        endPoint: UnitPoint(x: 1.34, y: 3.1)
+                    )
+                )
+                .cornerRadius(15, corners: [.topLeft, .topRight])
+
+
+            HStack{
+                Image(systemName: "barcode.viewfinder")
+                Text("바코드 스캔하기")
+            }
+            .bold()
+            .foregroundColor(.white)
+
+        }
+        .padding(.bottom, 30)
     }
 }
 
