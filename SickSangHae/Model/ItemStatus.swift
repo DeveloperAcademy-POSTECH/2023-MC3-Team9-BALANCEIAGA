762d3805566366b4063dff62721657e0c374b736
Lets <ruvan123@naver.com>
2023-07-13 17:36:38 +0900

    feature: setup basic model properties

---
+++ SickSangHae/Model/ItemStatus.swift
@@ -0,0 +1,8 @@
+//
+//  ItemStatus.swift
+//  SickSangHae
+//
+//  Created by user on 2023/07/13.
+//
+
+import Foundation
---
+++ SickSangHae/Model/Receipt+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Receipt+CoreDataProperties.swift
+//  SickSangHae
+//
+//  Created by user on 2023/07/13.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Receipt {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Receipt> {
+        return NSFetchRequest<Receipt>(entityName: "Receipt")
+    }
+
+    @NSManaged public var state: Int16
+    @NSManaged public var price: Double
+    @NSManaged public var name: String
+    @NSManaged public var id: UUID
+    @NSManaged public var icon: Data
+    @NSManaged public var fastEatPin: Bool
+    @NSManaged public var dateOfPurchase: Date
+    @NSManaged public var placeOfPurchase: String?
+
+    var status: Status {
+        get {
+            return Status(rawValue: self.state) ?? .UnConsumed
+        }
+        set {
+            self.state = newValue.rawValue
+        }
+    }
+}
+
+extension Receipt : Identifiable {
+
+}
