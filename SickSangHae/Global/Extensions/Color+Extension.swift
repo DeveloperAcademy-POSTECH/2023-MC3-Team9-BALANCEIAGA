c701cd5c0217d359257887452dddc5d11dd2c519
Reene <vernum.amor@gmail.com>
2023-07-14 17:31:39 +0900

    feature: add color extension
    feature: add color extension

--- SickSangHae/Global/Extensions/Color+Extension.swift
+++ SickSangHae/Global/Extensions/Color+Extension.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 extension Color {
+    public static let lightGreenGrayColor = Color("LightGreenGray")
     public static let lightGrayColor = Color("LightGray")
-    public static let tempGrayColor = Color("TempGray")
-    public static let whatGrayColor = Color("WhatGray")
+    public static let lightBlueGrayColor = Color("LightBlueGray")
+    public static let blueGrayColor = Color("BlueGray")
     
     
 }
