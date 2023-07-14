26a43dfc2876d68169196e529cd4390e6e2f9679
Reene <vernum.amor@gmail.com>
2023-07-14 17:32:39 +0900

    feature: update ItemBlockView.swift
    add xmark, change color

--- SickSangHae/View/Reusable/ItemBlockView.swift
+++ SickSangHae/View/Reusable/ItemBlockView.swift
@@ -9,30 +9,37 @@ import SwiftUI
 
 struct ItemBlockView: View {
     var body: some View {
+        
         ZStack {
             Rectangle()
                 .frame(width: 350, height: 116)
-                .foregroundColor(.tempGrayColor)
+                .foregroundColor(.lightGrayColor)
                 .cornerRadius(12)
             VStack(alignment: .leading) {
                 HStack {
                     Text("품목")
                         .padding(.leading,20)
                     Text("품목을 입력해주세요.")
-                        .foregroundColor(.lightGrayColor)
+                        .foregroundColor(.blueGrayColor)
                         .padding(.leading,30)
                 }
                 Divider()
                     .frame(width: 350, height: 10)
-                    .foregroundColor(.whatGrayColor)
+                    .foregroundColor(.lightGrayColor)
                 HStack {
                     Text("금액")
                         .padding(.leading,20)
                     Text("금액을 입력해주세요.")
-                        .foregroundColor(.lightGrayColor)
+                        .foregroundColor(.blueGrayColor)
                         .padding(.leading,30)
                 }
             }
+            Image(systemName: "xmark.circle.fill")
+                .resizable()
+                .frame(width:25 ,height:25)
+                .foregroundColor(.lightBlueGrayColor)
+                .padding(.leading, 295)
+                .padding(.bottom, 60)
         }
     }
 }
