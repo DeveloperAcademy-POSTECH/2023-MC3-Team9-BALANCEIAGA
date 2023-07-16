dfaf00d0dcb2ebb6fddbd7525442a11cf591e602
wonniiii <eve652358@gmail.com>
2023-07-16 21:32:42 +0900

    feat: add CameraView.swift

--- SickSangHae/View/CameraView.swift
+++ SickSangHae/View/CameraView.swift
@@ -6,15 +6,55 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct CameraView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @ObservedObject var viewModel = CameraViewModel()
+  
+  var body: some View {
+    ZStack {
+      viewModel.cameraPreview.ignoresSafeArea()
+        .onAppear {
+          viewModel.configure()
+        }
+        .gesture(MagnificationGesture()
+          .onChanged { val in
+            viewModel.zoom(factor: val)
+          }
+          .onEnded { _ in
+            viewModel.zoomInitialize()
+          }
+        )
+      VStack {
+        Spacer()
+        HStack {
+          Button {
+            viewModel.imagePickerPresented.toggle()
+          } label: {
+            Image("ic_gallery")
+              .padding(.leading, 32.adjusted)
+          }
+          Spacer()
+          Button(action: {
+            viewModel.capturePhoto()}) {
+              Image("img_cameraShutter")
+            }
+          Spacer()
+          Button(action: {viewModel.switchFlash()}) {
+            Image(systemName: viewModel.isFlashOn ?
+                  "bolt.fill" : "bolt.slash")
+            .foregroundColor(.white)
+          }
+        }
+      }
+      .foregroundColor(.white)
+      /// ImagePicker
+      .sheet(isPresented: $viewModel.imagePickerPresented, onDismiss: {
+        viewModel.OCRViewPresented.toggle()
+      }) {
+        ImagePicker(image: $viewModel.selectedImage, isPresented: $viewModel.imagePickerPresented)
+      }
     }
+  }
 }
 
-struct CameraView_Previews: PreviewProvider {
-    static var previews: some View {
-        CameraView()
-    }
-}
