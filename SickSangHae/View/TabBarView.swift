e46aa91154885a802e98386bcfccf85c5369b35e
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-17 17:41:46 +0900

    feature: update TabBarView
    implement basic structure

--- SickSangHae/View/TabBarView.swift
+++ SickSangHae/View/TabBarView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct TabBarView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    // views에 다른 뷰를 넣으면 돼요. 현재는 알아보기 쉽게하기 위해 color를 넣어 두었어요.
+    let views: [Color] = [ .yellow, .blue, .green, .indigo]
+    let icons = ["bag.fill", "chart.bar", "newspaper.fill", "circle.inset.filled"]
+    let tabbarItems = [ "나의 냉장고", "통계", "보관함", "설정" ]
+
+        var body: some View {
+            TabView {
+                ForEach(views.indices, id: \.self) { index in
+                    views[index]
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .tag(index)
+                        .tabItem {
+                            Image(systemName: icons[index])
+                            Text(tabbarItems[index])
+                        }
+                }
+            }
+        }
 }
 
 struct TabBarView_Previews: PreviewProvider {
