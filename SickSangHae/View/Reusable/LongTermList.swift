e65e1f7453981b30dfdfb049b155eea59db5f8bb
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-18 23:01:48 +0900

    feature: update LongTermListView
    feature: update LongTermListView

--- SickSangHae/View/Reusable/LongTermList.swift
+++ SickSangHae/View/Reusable/LongTermList.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct LongTermList: View {
+    let testLists = ["계란 30구","모둠쌈","요플레","요플레","모둠쌈","라면","양파","계란","모둠쌈","요플레","모둠쌈","모둠쌈","계란","요플레","모둠쌈","요플레"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(testLists, id: \.self) { testList in
+
+                    HStack{
+
+                        // 이 부분 빈 문자열이 없으면 이미지 밑에 구분선이 왜 없어질까요?
+                        Text("")
+                            .foregroundColor(.clear)
+
+                        Image(systemName: "circle.fill")
+                            .resizable()
+                            .foregroundColor(Color("Gray200"))
+                            .frame(width: 36.adjusted, height: 36.adjusted)
+
+                        Text(testList)
+                            .foregroundColor(Color("Gray900"))
+                            .font(.system(size: 17.adjusted).weight(.semibold))
+                        //                            .font(.custom("Pretendard-Bold", size: 20)) // Pretendard Bold 폰트 적용
+
+                        Spacer()
+
+                        Text("구매한지 x일")
+                            .foregroundColor(Color("Gray900"))
+                            .font(.system(size: 14.adjusted).weight(.semibold))
+                    }
+
+                }
+                .padding(.vertical, 16.adjusted)
+            }
+            .listStyle(.plain)
+            .padding(.top, 27.adjusted)
+            .navigationBarItems(leading: leadingText, trailing: trailingButton)
+        }
+
     }
+
+    var leadingText: some View {
+            Text("천천히 먹어도 돼요")
+            .foregroundColor(Color("Gray900"))
+            .font(.system(size: 20.adjusted).weight(.bold))
+            .padding(.top, 27.adjusted)
+        }
+
+    var trailingButton: some View {
+        Button(action: {
+            // Your action for the trailing button
+        }) {
+            Text("최신순")
+            Image(systemName: "arrow.up.arrow.down")
+        }
+        .foregroundColor(Color("Gray600"))
+        .font(.system(size: 14.adjusted).weight(.semibold))
+        .padding(.top, 27.adjusted)
+    }
+
+
 }
 
 struct LongTermList_Previews: PreviewProvider {
