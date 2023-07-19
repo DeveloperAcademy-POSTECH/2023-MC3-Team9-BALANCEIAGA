a14675f712d69a0ea5b9a239c496fe3e0e3ee644
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-19 20:08:55 +0900

    fix: change NavigationStack to List
    fix: change NavigationStack to List

--- SickSangHae/View/Reusable/LongTermList.swift
+++ SickSangHae/View/Reusable/LongTermList.swift
@@ -11,10 +11,20 @@ struct LongTermList: View {
     let testLists = ["계란 30구","모둠쌈","요플레","요플레","모둠쌈","라면","양파","계란","모둠쌈","요플레","모둠쌈","모둠쌈","계란","요플레","모둠쌈","요플레"]
 
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(testLists, id: \.self) { testList in
 
+
+            List {
+
+                HStack{
+                    Text("천천히 먹어도 돼요")
+
+                    Spacer()
+
+                    Text("최신순")
+                }
+//                .padding([.top,.bottom], 5.adjusted)
+
+                ForEach(testLists, id: \.self) { testList in
                     HStack{
 
                         // 이 부분 빈 문자열이 없으면 이미지 밑에 구분선이 왜 없어질까요?
@@ -42,32 +52,9 @@ struct LongTermList: View {
                 .padding(.vertical, 16.adjusted)
             }
             .listStyle(.plain)
-            .padding(.top, 27.adjusted)
-            .navigationBarItems(leading: leadingText, trailing: trailingButton)
-        }
+
 
     }
-
-    var leadingText: some View {
-            Text("천천히 먹어도 돼요")
-            .foregroundColor(Color("Gray900"))
-            .font(.system(size: 20.adjusted).weight(.bold))
-            .padding(.top, 27.adjusted)
-        }
-
-    var trailingButton: some View {
-        Button(action: {
-            // Your action for the trailing button
-        }) {
-            Text("최신순")
-            Image(systemName: "arrow.up.arrow.down")
-        }
-        .foregroundColor(Color("Gray600"))
-        .font(.system(size: 14.adjusted).weight(.semibold))
-        .padding(.top, 27.adjusted)
-    }
-
-
 }
 
 struct LongTermList_Previews: PreviewProvider {
