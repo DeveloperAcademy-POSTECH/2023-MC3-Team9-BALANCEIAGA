bf9ad4ecf2a012c5b6a6043253078c5bae6bf428
Lets <ruvan123@naver.com>
2023-07-20 02:16:41 +0900

    refactor: remove NavigationStack from BasicListView.swift

--- SickSangHae/View/Reusable/BasicList.swift
+++ SickSangHae/View/Reusable/BasicList.swift
@@ -8,15 +8,14 @@
 import SwiftUI
 
 struct BasicList: View {
-    @StateObject var coreDataViewModel = CoreDataViewModel()
-    @State private var ascending = false
     
     var body: some View {
         VStack {
-            NavigationStack {
-                ListViewTemplate(title: "빨리 먹어야 해요 🕖", buttonTitle: "최신순", coreDataViewModel:
-                                    coreDataViewModel)
+            List {
+                PinnedListTitle(title: "빨리 먹어야 해요 🕖")
+                ListContent()
             }
+            .listStyle(.plain)
             
             Rectangle()
             .foregroundColor(.clear)
@@ -24,83 +23,83 @@ struct BasicList: View {
             .background(.gray)
 //            .background("Gray100")
             
-            NavigationStack {
-                ListViewTemplate(title: "기본", buttonTitle: "최신순", coreDataViewModel: coreDataViewModel)
+            List {
+                BasicListTitle(title: "기본")
+                ListContent()
             }
-            
-            Button("코어데이터 만들기") {
-                coreDataViewModel.createReceiptData()
-                print(coreDataViewModel.receipts.count)
-            }
+            .listStyle(.plain)
         }
     }
-    
-    struct ListViewTemplate: View {
-        let title: String
-        let buttonTitle: String
-        @ObservedObject var coreDataViewModel:  CoreDataViewModel
-        
-        var body: some View {
-            VStack {
-                List {
-                    ForEach(coreDataViewModel.receipts, id: \.self) { testList in
-                        
-                        HStack{
-                            
-                            // 이 부분 빈 문자열이 없으면 이미지 밑에 구분선이 왜 없어질까요?
-                            Text("")
-                                .foregroundColor(.clear)
-                            
-                            Image(systemName: "circle.fill")
-                                .resizable()
-                            //                                   .foregroundColor(Color("Gray200"))
-                                .frame(width: 36.adjusted, height: 36.adjusted)
-                            
-                            Text(testList.name)
-                            //                                   .foregroundColor(Color("Gray900"))
-                                .font(.system(size: 17.adjusted).weight(.semibold))
-                            //                            .font(.custom("Pretendard-Bold", size: 20)) // Pretendard Bold 폰트 적용
-                            Spacer()
-                            
-                            Text("구매한지 x일")
-                            //                                   .foregroundColor(Color("Gray900"))
-                                .font(.system(size: 14.adjusted).weight(.semibold))
-                        }
-                    }
-                    .padding(.vertical, 16.adjusted)
-                }
-                .listStyle(.plain)
-                .padding(.top, 27.adjusted)
-                .navigationBarItems(leading: leadingText(title: title), trailing: trailingButton(buttonTitle: buttonTitle))
-            }
-        }
-    }
-    
-    struct leadingText: View {
-        let title: String
-        var body: some View {
-            Text(title)
-            //                   .foregroundColor(Color("Gray900"))
-                .font(.system(size: 20.adjusted).weight(.bold))
-                .padding(.top, 27.adjusted)
-        }
-    }
+}
 
-    struct trailingButton: View {
-        let buttonTitle: String
-        var body: some View {
-            Button(action: {
-                // Your action for the trailing button
-            }) {
-                Text(buttonTitle)
-                Image(systemName: "arrow.up.arrow.down")
+private struct BasicListTitle: View {
+    
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+//                .foregroundColor(Color("Gray900"))
+                .font(.system(size: 20.adjusted))
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                HStack(spacing: 2.adjusted) {
+                    Text("최신순")
+                    Image(systemName: "arrow.up.arrow.down")
+                }
             }
-            //               .foregroundColor(Color("Gray600"))
-            .font(.system(size: 14.adjusted).weight(.semibold))
-            .padding(.top, 27.adjusted)
+//            .foregroundColor(Color("Gray600"))
+            .font(.system(size: 14.adjusted))
         }
     }
+}
+
+private struct PinnedListTitle: View {
     
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+//                .foregroundColor(Color("Gray900"))
+                .font(.system(size: 20.adjusted))
+            
+            Spacer()
+        }
+    }
+}
+
+
+private struct ListContent: View {
+    let itemList: [Receipt] = []
+    var body: some View {
+        VStack {
+            ForEach(itemList, id: \.self) { receipt in
+                HStack {
+                    Text("")
+                        .foregroundColor(.clear)
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                    //                .foregroundColor(Color("Gray200"))
+                        .frame(width: 36.adjusted, height: 36.adjusted)
+                    
+                    Text(receipt.name)
+                        .foregroundColor(.clear)
+                    
+                    Spacer()
+                    
+                    Text("구매한지 x일")
+                    //                .foregroundColor(Color("Gray900"))
+                        .font(.system(size: 14.adjusted))
+                }
+            }
+        }
+    }
 }
 
 struct BasicList_Previews: PreviewProvider {
