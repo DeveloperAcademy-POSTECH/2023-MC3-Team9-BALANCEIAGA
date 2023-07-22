56a90c13f3282e4df953c85d424238db9e8a0fc6
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-23 00:27:01 +0900

    feature: pie chart in ChartView
    feature: pie chart in ChartView

--- SickSangHae/View/ChartView.swift
+++ SickSangHae/View/ChartView.swift
@@ -59,7 +59,14 @@ struct ChartView: View {
                 }
             }
 
+            Spacer()
+                .frame(height: 41.adjusted)
 
+            Pie(slices: [
+                (7, Color("PrimaryGB")),
+                (3, Color("Gray200"))
+                ])
+            .frame(width: 250.adjusted)
 
             Spacer()
 
@@ -74,6 +81,45 @@ struct ChartView: View {
            return String(month)
        }
 }
+
+struct Pie: View {
+
+    @State var slices: [(Double, Color)]
+
+    var body: some View {
+        Canvas { context, size in
+            let donut = Path { p in
+                p.addEllipse(in: CGRect(origin: .zero, size: size))
+                p.addEllipse(in: CGRect(x: size.width * 0.25, y: size.height * 0.25, width: size.width * 0.5, height: size.height * 0.5))
+            }
+            context.clip(to: donut, style: .init(eoFill: true))
+
+            let total = slices.reduce(0) { $0 + $1.0 }
+            context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
+            var pieContext = context
+            pieContext.rotate(by: .degrees(-90))
+            let radius = min(size.width, size.height) * 0.48
+            var startAngle = Angle.zero
+            for (value, color) in slices {
+                let angle = Angle(degrees: 360 * (value / total))
+                let endAngle = startAngle + angle
+
+
+                let path = Path { p in
+                    p.move(to: .zero)
+                    p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+                    p.closeSubpath()
+                }
+                pieContext.fill(path, with: .color(color))
+
+                startAngle = endAngle
+            }
+
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+}
+
 
 struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
