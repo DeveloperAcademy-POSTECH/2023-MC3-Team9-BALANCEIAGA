81e9103c969ab658be5ebd06193b829cdbcf4ef7
wonniiii <eve652358@gmail.com>
2023-07-22 21:34:48 +0900

    feat: update TopAlertView.swift

--- SickSangHae/View/Reusable/TopAlertView.swift
+++ SickSangHae/View/Reusable/TopAlertView.swift
@@ -7,14 +7,46 @@
 
 import SwiftUI
 
-struct TopAlertView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+//MARK: - 색, 폰트 변경
 
-struct TopAlertView_Previews: PreviewProvider {
-    static var previews: some View {
-        TopAlertView()
+struct TopAlertView: ViewModifier {
+  var iconImage: String
+  var message: String
+  var backgroundColor: Color
+  @State private var isAlertVisible = true
+  
+  func body(content: Content) -> some View {
+    if isAlertVisible {
+      ZStack(alignment: .leading) {
+        backgroundColor
+        HStack(spacing: 14.adjusted) {
+          Image(iconImage)
+            .resizable()
+            .frame(width: 40.adjusted, height: 40.adjusted)
+            .scaledToFit()
+          VStack(spacing: 4.adjusted) {
+            Text(message)
+              .font(.system(size: 14).bold())
+              .foregroundColor(.white)
+            Text("눌러서 실행 취소")
+              .font(.system(size: 12))
+              .foregroundColor(.gray)
+          }
+        }
+        .frame(width: 348.adjusted, height: 68.adjusted)
+        .background(.regularMaterial)
+        .cornerRadius(41.adjusted)
+      }
+      .animation(.easeInOut)
+      .gesture (DragGesture()
+        .onChanged { value in
+          if value.translation.height < -50 {
+            withAnimation {
+              isAlertVisible = false
+            }
+          }
+        }
+      )
     }
+  }
 }
