1af0f41fc75d883580d7d1f8a68a228a304bebf0
wonniiii <eve652358@gmail.com>
2023-07-23 04:25:14 +0900

    refactor: fix viewModel

--- SickSangHae/ViewModel/TopAlertViewModel.swift
+++ SickSangHae/ViewModel/TopAlertViewModel.swift
@@ -8,31 +8,33 @@
 import SwiftUI
 
 class TopAlertViewModel: ObservableObject {
-    @Published var isAlertVisible = true
-    var name: String
-    var currentCase: TopAlertViewCase
-
-    init(name: String, currentCase: TopAlertViewCase) {
-        self.name = name
-        self.currentCase = currentCase
+  @Published var isAlertVisible = true
+  var name: String
+  var currentCase: TopAlertViewCase
+  
+  @State private var dragOffset = CGSize.zero
+  
+  init(name: String, currentCase: TopAlertViewCase) {
+    self.name = name
+    self.currentCase = currentCase
+  }
+  
+  func hideAlert() {
+    withAnimation {
+      isAlertVisible = false
     }
-
-    func hideAlert() {
-        withAnimation {
-            isAlertVisible = false
-        }
-    }
-
+  }
+  
   // 제스처 변경(스와이프) 처리를 수행하는 함수
-    func onDragChanged(value: DragGesture.Value) {
-        let translation = value.translation
-    }
+  func onDragChanged(value: DragGesture.Value) {
+    dragOffset = value.translation
+  }
   
   // 제스처 종료(스와이프 완료) 처리를 수행하는 함수
-    func onDragEnded(value: DragGesture.Value) {
-        let translation = value.translation
-        if translation.height < -50 {
-            hideAlert()
-        }
+  func onDragEnded(value: DragGesture.Value) {
+    dragOffset = CGSize.zero
+    if value.translation.height < -50 {
+      hideAlert()
     }
+  }
 }
