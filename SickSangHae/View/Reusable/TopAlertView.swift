a787dd4aabe073594a3c11c00e2ed9c7530517eb
wonniiii <eve652358@gmail.com>
2023-07-23 02:37:06 +0900

    feat: update topAlertView detail function

--- SickSangHae/View/Reusable/TopAlertView.swift
+++ SickSangHae/View/Reusable/TopAlertView.swift
@@ -22,11 +22,11 @@ struct TopAlertView: View {
   var body: some View {
     switch currentCase {
     case .delete:
-      TopAlertBaseView(iconImage: "img_delete", message: "\(name)를 삭제했어요", backgroundColor: .red)
+      TopAlertBaseView(iconImage: "img_delete", message: "\(name)를 삭제했어요", backgroundColor: .pointRLight, strokeColor: .pointRMiddle)
     case .rot:
-      TopAlertBaseView(iconImage: "img_rot", message: "\(name)가 상했어요", backgroundColor: .red)
+      TopAlertBaseView(iconImage: "img_rot", message: "\(name)가 상했어요", backgroundColor: .pointRLight, strokeColor: .pointRMiddle)
     case .eat:
-      TopAlertBaseView(iconImage: "img_eat", message: "\(name)를 먹었어요", backgroundColor: .green)
+      TopAlertBaseView(iconImage: "img_eat", message: "\(name)를 먹었어요", backgroundColor: .alertGreen, strokeColor: .primaryGMiddle)
   
     }
   }
@@ -36,6 +36,7 @@ struct TopAlertBaseView: View {
   var iconImage: String
   var message: String
   var backgroundColor: Color
+  var strokeColor: Color
   @State private var isAlertVisible = true
   @GestureState private var dragOffset = CGSize.zero
 
@@ -43,12 +44,12 @@ struct TopAlertBaseView: View {
     if isAlertVisible {
       ZStack(alignment: .leading) {
         RoundedRectangle(cornerRadius: 41.adjusted)
-          .stroke(backgroundColor, lineWidth: 1)
-          .background(.regularMaterial)
+          .stroke(strokeColor, lineWidth: 1)
           .background(backgroundColor)
-          .opacity(0.7)
-          .frame(width: 348.adjusted, height: 68.adjusted)
+          .background(.ultraThickMaterial)
+          .frame(height: 68.adjusted)
           .clipShape(RoundedRectangle(cornerRadius: 41.adjusted))
+          .opacity(0.8)
         HStack(spacing: 10.adjusted) {
           Image(iconImage)
             .resizable()
@@ -65,6 +66,8 @@ struct TopAlertBaseView: View {
         }
         .padding(.leading, 14.adjusted)
       }
+      .padding([.leading, .trailing], 20.adjusted)
+
       .animation(.easeInOut)
       .offset(y: max(dragOffset.height, 0))
       .gesture (DragGesture()
