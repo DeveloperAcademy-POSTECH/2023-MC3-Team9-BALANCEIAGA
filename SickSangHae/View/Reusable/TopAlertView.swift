8b057c6e53095f44f8c56228a9a3562f75dd7613
wonniiii <eve652358@gmail.com>
2023-07-23 01:44:31 +0900

    feat: complete TopAlertView function

--- SickSangHae/View/Reusable/TopAlertView.swift
+++ SickSangHae/View/Reusable/TopAlertView.swift
@@ -9,44 +9,83 @@ import SwiftUI
 
 //MARK: - 색, 폰트 변경
 
-struct TopAlertView: ViewModifier {
+enum TopAlertViewCase {
+  case delete
+  case rot
+  case eat
+}
+
+struct TopAlertView: View {
+  var name: String
+  var currentCase: TopAlertViewCase = .delete
+
+  var body: some View {
+    switch currentCase {
+    case .delete:
+      TopAlertBaseView(iconImage: "img_delete", message: "\(name)를 삭제했어요", backgroundColor: .red)
+    case .rot:
+      TopAlertBaseView(iconImage: "img_rot", message: "\(name)가 상했어요", backgroundColor: .red)
+    case .eat:
+      TopAlertBaseView(iconImage: "img_eat", message: "\(name)를 먹었어요", backgroundColor: .green)
+  
+    }
+  }
+}
+
+struct TopAlertBaseView: View {
   var iconImage: String
   var message: String
   var backgroundColor: Color
   @State private var isAlertVisible = true
-  
-  func body(content: Content) -> some View {
+  @GestureState private var dragOffset = CGSize.zero
+
+  var body: some View {
     if isAlertVisible {
       ZStack(alignment: .leading) {
-        backgroundColor
-        HStack(spacing: 14.adjusted) {
+        RoundedRectangle(cornerRadius: 41.adjusted)
+          .stroke(backgroundColor, lineWidth: 1)
+          .background(.regularMaterial)
+          .background(backgroundColor)
+          .opacity(0.7)
+          .frame(width: 348.adjusted, height: 68.adjusted)
+          .clipShape(RoundedRectangle(cornerRadius: 41.adjusted))
+        HStack(spacing: 10.adjusted) {
           Image(iconImage)
             .resizable()
-            .frame(width: 40.adjusted, height: 40.adjusted)
-            .scaledToFit()
-          VStack(spacing: 4.adjusted) {
+            .scaledToFill()
+            .frame(width: 44.adjusted, height: 44.adjusted)
+          VStack(alignment: .leading, spacing: 4.adjusted) {
             Text(message)
               .font(.system(size: 14).bold())
-              .foregroundColor(.white)
+              .foregroundColor(.black)
             Text("눌러서 실행 취소")
               .font(.system(size: 12))
               .foregroundColor(.gray)
           }
         }
-        .frame(width: 348.adjusted, height: 68.adjusted)
-        .background(.regularMaterial)
-        .cornerRadius(41.adjusted)
+        .padding(.leading, 14.adjusted)
       }
       .animation(.easeInOut)
+      .offset(y: max(dragOffset.height, 0))
       .gesture (DragGesture()
-        .onChanged { value in
+        .updating($dragOffset, body: { (value, dragOffset, _) in
+          dragOffset = value.translation
+        })
+        .onEnded { value in
           if value.translation.height < -50 {
             withAnimation {
               isAlertVisible = false
             }
           }
-        }
-      )
+        })
     }
   }
+}
+
+
+struct TopAlertView_Previews: PreviewProvider {
+    static var previews: some View {
+      TopAlertView(name: "파채", currentCase: .delete)
+
+    }
 }
