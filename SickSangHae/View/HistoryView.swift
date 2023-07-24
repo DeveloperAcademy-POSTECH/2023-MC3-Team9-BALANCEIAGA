79520bb0f0bcab6028c599fc71e24b8215713f9e
SangwooKim <mollangcow@gmail.com>
2023-07-24 15:14:42 +0900

    fix: complete HistoryView
    #12/implementHistoryView

--- SickSangHae/View/HistoryView.swift
+++ SickSangHae/View/HistoryView.swift
@@ -13,133 +13,145 @@ struct HistoryView: View {
     
     @State var isMovingSegmentedTab : Bool = true
     
-    let array = [
-        "당근", "계란30구", "과자", "팝콘", "요거트"
-    ]
+    let array = ["Apple", "Banana", "Orange", "Pineapple", "Grapes", "Watermelon"]
     
     var body: some View {
         NavigationStack {
-            ZStack(alignment: .top) {
+            VStack(spacing: 0) {
+                topHeader
                 
-                //히스토리 뷰 본체
-                VStack(spacing: 0) {
+                ScrollView {
+                    deleteNotiMessage
                     
-                    //상단 탭 영역
-                    topHeader
+                    listSection
                     
-                    //아이템 리스트 뷰
-                    List {
-                        
-                        //아이템 리스트 헤더 날짜
-                        listHeaderDate
-                        
-                        //아이템 리스트
-                        itemList
-                    }
-                    .listStyle(.plain)
-                }
-                .ignoresSafeArea()
-            } //ZStack닫기
+                    listSection
+                    
+                } //ScrollView닫기
+                .listStyle(.plain)
+                
+            } //VStack닫기
+            
         } //NavigationStack닫기
+        
     } //body닫기
     
+    
     var topHeader: some View {
-        //상단 탭 뷰
-        ZStack(alignment: .top) {
-            ZStack(alignment: .bottom) {
-                //백그라운드
-                headerBackground
-                
-                //버튼 및 타이틀
-                VStack(spacing: 0) {
-                    
-                    //타이틀
-                    titleText
-                    
-                    //Test버튼
-                    Button(action: {
-                        coreDataViewModel
-                        print(">>>AddModel")
-                    }, label: {
-                        Text("추가")
-                    })
-                    
-                    //버튼
-                    segmentedTabButton
-                    
-                    //하단구분선
-                    Divider()
-                } //VStack닫기
-                .frame(
-                    width: screenWidth)
-            }
-        } //ZStack닫기
-    }
-    
-    var headerBackground: some View {
-        Rectangle()
-            .foregroundColor(.white)
-            .frame(
-                width: screenWidth,
-                height: screenHeight * 0.2)
-            .ignoresSafeArea()
-    }
-    
-    var titleText: some View {
-        Text("보관함")
-            .font(.system(size: 28, weight: .bold))
-            .frame(
-                width: screenWidth * 0.9,
-                height: screenHeight * 0.07,
-                alignment: .topLeading)
-    }
+        //버튼 및 타이틀
+        VStack(alignment: .leading, spacing: 0) {
+            Spacer()
+                .frame(height: 32)
+            
+            Text("보관함")
+                .font(.system(size: 28, weight: .bold))
+                .foregroundColor(Color("PrimaryGB"))
+                .padding(.horizontal, 20)
+            
+            Spacer()
+                .frame(height: 20)
+            
+            segmentedTabButton
+                .padding(.horizontal, 20)
+            
+        } //VStack닫기
+        
+    } //topHeader닫기
     
     var segmentedTabButton: some View {
         HStack {
-            VStack {
+            VStack(spacing: 0) {
+                Spacer()
+                
                 Button(action: {
                     isMovingSegmentedTab = true
                 }, label: {
                     Text("먹었어요😋")
                         .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.black)
-                })
-                Rectangle()
-                    .foregroundColor(isMovingSegmentedTab ? .black : .clear)
-                    .frame(width: screenWidth * 0.36, height: 3.adjusted)
-            }
+                        .foregroundColor(Color("Gray900"))
+                }) //Button닫기
+                
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 1.5)
+                    .foregroundColor(isMovingSegmentedTab ? Color("PrimaryGB") : .clear)
+                    .frame(width: screenWidth * 0.4, height: 3)
+            } //VStack닫기
+            
             Spacer()
-            VStack {
+            
+            VStack(spacing: 0) {
+                Spacer()
+                
                 Button(action: {
                     isMovingSegmentedTab = false
                 }, label: {
                     Text("상했어요🤢")
                         .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.black)
-                })
-                Rectangle()
-                    .foregroundColor(isMovingSegmentedTab ? .clear : .black)
-                    .frame(width: screenWidth * 0.36, height: 3.adjusted)
-            }
+                        .foregroundColor(Color("Gray900"))
+                }) //Button닫기
+                
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 1.5)
+                    .foregroundColor(isMovingSegmentedTab ? .clear : Color("PrimaryGB"))
+                    .frame(width: screenWidth * 0.4, height: 3)
+            } //VStack닫기
         } //HStack닫기
-        .frame(
-            width: screenWidth * 0.8)
-    }
+        .frame(height: 52)
+    } //segmentedTabButton닫기
     
-    var listHeaderDate: some View {
+    
+    var deleteNotiMessage: some View {
+        VStack(alignment: .leading) {
+            Spacer()
+                .frame(height: 24)
+            
+            Text("아래의 항목들은 이곳에서 90일 동안 보관됩니다. 각 항목들은 90일이 지나면 영구적으로 삭제됩니다.")
+                .font(.system(size: 14))
+                .foregroundColor(Color("Gray600"))
+            
+        } //VStack닫기
+        .padding(.horizontal, 18)
+        
+    } //deleteNotiMessage닫기
+    
+    
+    var listSection: some View {
+        VStack(spacing: 0) {
+            listTitle
+            
+            itemList
+            
+            Spacer()
+                .frame(height: 4)
+            
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: screenWidth, height: 12)
+                .background(Color("Gray100"))
+            
+        } //VStack닫기
+        
+    } //listSection닫기
+    
+    var listTitle: some View {
         HStack {
             Text("2023년 7월 21일")
-                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(Color("Gray900"))
+                .font(.system(size: 20).weight(.semibold))
             
             Spacer()
             
             Text("88일 남음")
                 .font(.system(size: 14, weight: .medium))
-                .foregroundColor(.gray)
-        }
-        .frame(width: screenWidth * 0.9)
-        .padding(.top, 12.adjusted)
-    }
+                .foregroundColor(Color("Gray600"))
+            
+        } //HStack닫기
+        .padding([.top, .bottom], 17)
+        .padding([.leading, .trailing], 20)
+        
+    } //listTitle닫기
     
     var itemList: some View {
         ForEach(array, id:\.self) { item in
@@ -147,15 +159,18 @@ struct HistoryView: View {
                 HStack {
                     Image(systemName: "circle.fill")
                         .resizable()
+                        .foregroundColor(Color("Gray200"))
                         .frame(width: 36, height: 36)
-                        .foregroundColor(.gray)
-                    Text(item)
-                        .frame(
-                            height: 60,
-                            alignment: .leading)
-                        .font(.system(size: 17, weight: .bold))
-                        .padding(.leading, 8)
+                    
                     Spacer()
+                        .frame(width: 12)
+                    
+                    Text(item)
+                        .font(.system(size: 17).weight(.semibold))
+                        .foregroundColor(Color("Gray900"))
+                    
+                    Spacer()
+                    
                     Menu {
                         Button(action: {
                             //아이템 상태 복구 로직
@@ -163,37 +178,50 @@ struct HistoryView: View {
                             Text("복구하기")
                             Image(systemName: "arrow.counterclockwise")
                         })
+                        
                         Button(action: {
                             //아이템 상태 변경 로직
                         }, label: {
                             Text("상했어요")
                             Image(systemName: "arrow.triangle.2.circlepath")
                         })
+                        
                         Divider()
+                        
                         Button(role: .destructive, action: {
                             
                         }, label: {
                             Text("삭제하기")
-                            Image(systemName: "trash")
+                            Image(systemName: "trash.fill")
                         })
                     } label: {
-                        Image(systemName: "ellipsis")
-                            .resizable()
-                            .frame(width: 22, height: 5)
-                            .foregroundColor(.gray)
-                    }
+                        Rectangle()
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(.clear)
+                            .overlay(
+                                Image(systemName: "ellipsis")
+                                    .resizable()
+                                    .foregroundColor(Color("Gray200"))
+                                    .frame(width: 21, height: 5)
+                            )
+                            .padding(.trailing, 20)
+                    } //Menu닫기
+                    
                 } //HStack닫기
-                .frame(width: screenWidth * 0.9)
+                .padding(.top, 12)
                 
                 Divider()
-                    .frame(width: screenWidth * 0.9, alignment: .trailing)
-            }
-            .listRowSeparator(.hidden)
-            .frame(width: screenWidth)
+                    .overlay(Color("Gray100"))
+                    .opacity(item == array.last ? 0 : 1)
+                
+            } //VStack닫기
+            .padding(.leading, 20)
+            
         } //ForEach닫기
-        .padding(.vertical, -7.adjusted)
-    }
-}
+        
+    } //itemList닫기
+    
+} //struct닫기
 
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
