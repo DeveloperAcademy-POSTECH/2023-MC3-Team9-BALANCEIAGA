d77ba1d012b56dcb752b7fab7182ee061824fb9f
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-27 03:41:44 +0900

    feature: resolve OCRView GPT async problem
    feature: resolve OCRView GPT async problem

--- SickSangHae/View/Reusable/OCRView.swift
+++ SickSangHae/View/Reusable/OCRView.swift
@@ -8,33 +8,59 @@
 import SwiftUI
 
 struct OCRView: View {
-  @ObservedObject private var viewModel = OCRViewModel()
-  @Environment(\.dismiss) private var dismiss
-  var image: UIImage
-  @State private var isMovingDown = true
-  @State private var isShowBoundingBoxView = true
-  @State private var isShowScanView = false
-  @State private var isShowItemCheckView = false
-  let appState: AppState
+    @ObservedObject private var viewModel = OCRViewModel()
+    @Environment(\.dismiss) private var dismiss
+    var image: UIImage
+    @State private var isMovingDown = true
+    @State private var isShowBoundingBoxView = true
+    @State private var isShowScanView = false
+    @State private var isShowItemCheckView = false
+    @State private var gptAnswer: String = ""
+
+    let appState: AppState
+    let dispatchGroup = DispatchGroup()
     
-  var body: some View {
-    GeometryReader { geometry in
-      Image(uiImage: image)
-        .resizable()
-        .frame(width: geometry.size.width, height: geometry.size.height)
-      if isShowBoundingBoxView {
-        BoundingBoxView
-          .onAppear {
-            viewModel.recognizeText(image: image)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-              isShowBoundingBoxView = false
-              isShowScanView = true
+    var body: some View {
+        GeometryReader { geometry in
+            Image(uiImage: image)
+                .resizable()
+                .frame(width: geometry.size.width, height: geometry.size.height)
+            if isShowBoundingBoxView {
+                BoundingBoxView
+                    .onAppear {
+                        viewModel.recognizeText(image: image)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            isShowBoundingBoxView = false
+                            isShowScanView = true
+                        }
+                    }
             }
-          }
-      }
-      if isShowScanView {
-        NavigationLink(destination: ItemCheckView(appState: appState), isActive: $isShowItemCheckView) {
-          EmptyView()
+            if isShowScanView {
+                NavigationLink(destination: ItemCheckView(gptAnswer: $viewModel.gptAnswer, appState: appState), isActive: $isShowItemCheckView) {
+                    EmptyView()
+                }
+                .hidden()
+                scanView
+                  .onAppear {
+                      // MARK: DispatchGroup을 enter 합니다.
+                      dispatchGroup.enter()
+                      DispatchQueue.main.async {
+                          // MARK: viewModel.queryGPT 안에서 DispatchGroup를 leave 합니다.
+                          viewModel.queryGPT(prompts: viewModel.gptPrompt, dispatchGroup: dispatchGroup){ word in
+                              gptAnswer += word
+                              print(gptAnswer)
+                          }
+                      }
+
+                      dispatchGroup.notify(queue: .main) {
+                          // MARK: 여기에 DispatchGroup의 모든 작업이 완료되었을 때 실행할 로직 추가
+                          isShowScanView = false
+                          isShowItemCheckView = true
+                          print(gptAnswer)
+                          viewModel.gptAnswer = viewModel.convertToDictionary(text: gptAnswer)!
+                      }
+                  }
+            }
         }
         .navigationBarBackButtonHidden(true)
     }
@@ -68,18 +94,7 @@ struct OCRView: View {
                 }
             }
         }
-        
-          VStack(alignment: .center) {
-          Spacer()
-          Text("영수증 인식완료!")
-            .font(.system(size: 22).bold())
-            .padding(.bottom, 80.adjusted)
-            .foregroundColor(Color.accentColor)
-          
-        }
-      }
     }
-
     private var scanView: some View {
         GeometryReader { geometry in
             Image("img_scan")
