99bf402dd0867c9dad1704952d5fa186467cb5ee
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-30 21:59:36 +0900

    feature: Chart pie connect coredata
    feature: Chart pie connect coredata

--- SickSangHae/View/ChartView.swift
+++ SickSangHae/View/ChartView.swift
@@ -11,6 +11,7 @@ struct ChartView: View {
     @State private var selectedDate = Date()
     @State private var wholeCost: Double = 1
     @State private var eatenCost: Double = 1
+    @State private var slices = [(2.0, Color("PrimaryGB")), (1.0, Color("Gray200"))]
 
     @EnvironmentObject var coreDataViewModel: CoreDataViewModel
 
@@ -24,7 +25,7 @@ struct ChartView: View {
     let data = ["Apple", "Banana", "Orange"]
 
     var body: some View {
-
+        
         Spacer()
             .frame(height: 32.adjusted)
 
@@ -73,64 +74,100 @@ struct ChartView: View {
                 Spacer()
                     .frame(height: 41.adjusted)
 
-                Pie(wholeCost: wholeCost, eatenCost: eatenCost)
+                Pie
                 .frame(width: 200.adjusted)
 
                 summaryList
                     .padding(.horizontal, 20.adjusted)
                     .padding(.vertical, 38.adjusted)
 
-                Group{
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: screenWidth, height: 12.adjusted)
-                        .background(Color("Gray100"))
-
-                    ListTitle(title: "잘 먹은 BEST3")
-                    ListContents(data: data)
-
-                    ListTitle(title: "못 먹은 WORST3")
-                    ListContents(data: data)
-
-                    ForEach(coreDataViewModel.receipts, id:\.self){ item in
-                        Text(item.name)
-                        Button(){
-                            print(dateFormatter.string(from: item.dateOfPurchase))
-                            print(item.price)
-                        }label:{
-                            Text("button")
-                        }
-                    }
-                }
-
+                // MARK: 디자인에서 이 부분 변경 사항이 있으면 주석을 해제하고 작업 할 것.
+//                Group{
+//                    Rectangle()
+//                        .foregroundColor(.clear)
+//                        .frame(width: screenWidth, height: 12.adjusted)
+//                        .background(Color("Gray100"))
+//
+//                    ListTitle(title: "잘 먹은 BEST3")
+//                    ListContents(data: data)
+//
+//                    ListTitle(title: "못 먹은 WORST3")
+//                    ListContents(data: data)
+//                }
             }
             .onChange(of: selectedDate){ newDate in
                 calculateCosts(for: newDate)
             }
         }
-        .onAppear {
-            calculateCosts(for: selectedDate)
-            print((wholeCost - eatenCost) / wholeCost)
-        }
     }
 
-    private func calculateCosts(for date: Date) {
-            let month = dateFormatter.string(from: date)
-            var newWholeCost: Double = 0
-            var newEatenCost: Double = 0
+    private var Pie : some View {
+            Canvas { context, size in
+                let donut = Path { p in
+                    let donutSize = CGSize(width: size.width * 0.3, height: size.height * 0.3) // Reduce the size by 0.8
+                                p.addEllipse(in: CGRect(origin: .zero, size: size))
+                                p.addEllipse(in: CGRect(x: (size.width - donutSize.width) / 2,
+                                                        y: (size.height - donutSize.height) / 2,
+                                                        width: donutSize.width, height: donutSize.height))
+                }
+                context.clip(to: donut, style: .init(eoFill: true))
 
-            for item in coreDataViewModel.receipts {
-                if month == dateFormatter.string(from: item.dateOfPurchase) {
-                    newWholeCost += item.price
-                    if item.currentStatus == .Eaten {
-                        newEatenCost += item.price
+                let total = slices.reduce(0) { $0 + $1.0 }
+                context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
+                var pieContext = context
+                pieContext.rotate(by: .degrees(-90))
+                let radius = min(size.width, size.height) * 0.48
+                var startAngle = Angle.zero
+                for (value, color) in slices {
+                    let angle = Angle(degrees: 360 * (value / total))
+                    let endAngle = startAngle + angle
+
+
+                    let path = Path { p in
+                        p.move(to: .zero)
+                        p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+                        p.closeSubpath()
                     }
+                    pieContext.fill(path, with: .color(color))
+
+                    startAngle = endAngle
+                }
+
+            }
+            .onChange(of: selectedDate){ _ in
+                calculateCosts(for: selectedDate)
+            }
+            .onAppear(){
+                calculateCosts(for: selectedDate)
+            }
+            .aspectRatio(1, contentMode: .fit)
+        }
+
+    private func calculateCosts(for date: Date) {
+        let month = dateFormatter.string(from: date)
+        var newWholeCost: Double = 0
+        var newEatenCost: Double = 0
+
+        for item in coreDataViewModel.receipts {
+            if month == dateFormatter.string(from: item.dateOfPurchase) {
+                newWholeCost += item.price
+                if item.currentStatus == .Eaten {
+                    newEatenCost += item.price
                 }
             }
-
-            wholeCost = newWholeCost
-            eatenCost = newEatenCost
         }
+
+        wholeCost = newWholeCost
+        eatenCost = newEatenCost
+        if wholeCost == 0, eatenCost == 0{
+            slices = [(0, Color("PrimaryGB")),
+                      (1, Color("Gray200"))]
+        }
+        else{
+            slices = [((wholeCost - eatenCost) / wholeCost, Color("PrimaryGB")),
+                      (1 - (wholeCost - eatenCost) / wholeCost, Color("Gray200"))]
+        }
+    }
 
     private func ListTitle(title: String) -> some View{
         HStack {
@@ -219,59 +256,6 @@ struct ChartView: View {
            return String(Calendar.current.component(.month, from: Date()))
        }
 }
-
-struct Pie: View {
-    @State var wholeCost: Double
-    @State var eatenCost: Double
-
-    var slices: [(Double, Color)] {
-            return [
-                ((wholeCost - eatenCost) / wholeCost, Color("PrimaryGB")),
-                (1, Color("Gray200"))
-            ]
-        }
-
-    var body: some View {
-        Canvas { context, size in
-            let donut = Path { p in
-                let donutSize = CGSize(width: size.width * 0.3, height: size.height * 0.3) // Reduce the size by 0.8
-                            p.addEllipse(in: CGRect(origin: .zero, size: size))
-                            p.addEllipse(in: CGRect(x: (size.width - donutSize.width) / 2,
-                                                    y: (size.height - donutSize.height) / 2,
-                                                    width: donutSize.width, height: donutSize.height))
-            }
-            context.clip(to: donut, style: .init(eoFill: true))
-
-            let total = slices.reduce(0) { $0 + $1.0 }
-            context.translateBy(x: size.width * 0.5, y: size.height * 0.5)
-            var pieContext = context
-            pieContext.rotate(by: .degrees(-90))
-            let radius = min(size.width, size.height) * 0.48
-            var startAngle = Angle.zero
-            for (value, color) in slices {
-                let angle = Angle(degrees: 360 * (value / total))
-                let endAngle = startAngle + angle
-
-
-                let path = Path { p in
-                    p.move(to: .zero)
-                    p.addArc(center: .zero, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-                    p.closeSubpath()
-                }
-                pieContext.fill(path, with: .color(color))
-
-                startAngle = endAngle
-            }
-
-        }
-        .onAppear(){
-            print("pie")
-            print(wholeCost, eatenCost)
-        }
-        .aspectRatio(1, contentMode: .fit)
-    }
-}
-
 
 private struct BasicListTitle: View {
 
