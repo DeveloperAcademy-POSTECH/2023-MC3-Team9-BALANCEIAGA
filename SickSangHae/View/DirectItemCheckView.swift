afeb89fcb7560e24dee3ec7df13452896dbe74c0
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-31 15:59:26 +0900

    fix: fix move to camera error
    fix: fix move to camera error

--- SickSangHae/View/DirectItemCheckView.swift
+++ SickSangHae/View/DirectItemCheckView.swift
@@ -9,12 +9,13 @@
 import SwiftUI
 
 struct DirectItemCheckView: View {
-    
+
     @Environment(\.dismiss) var dismiss
     @ObservedObject var viewModel: UpdateItemViewModel
     @State var appState: AppState
     @EnvironmentObject var coreDataViewModel: CoreDataViewModel
-    
+    @State private var isShowingModal = false
+
     var body: some View {
         NavigationStack {
             ZStack(alignment: .top) {
@@ -26,11 +27,11 @@ struct DirectItemCheckView: View {
                                 .frame(width: 10, height: 19)
                                 .foregroundColor(.gray900)
                         })
-                        
+
                         Spacer()
                         Button(action: {
                             self.appState.moveToRootView = true
-                            
+
                         }, label: {
                             Image(systemName: "xmark")
                                 .resizable()
@@ -38,22 +39,25 @@ struct DirectItemCheckView: View {
                         })
                         .foregroundColor(.gray900)
                     }.padding(.horizontal, 20)
-                    
+
                     HStack {
                         Text("아래 식료품을 등록할게요")
                             .fontWeight(.bold)
                             .padding(34)
                     }
-                    
+
                     ListTitle
-                    
+
                     ScrollView{
                         ListContents
                     }
-                    
+
                     Spacer()
-                    NavigationLink(destination: RegisterCompleteView(appState: appState) ,label: {
-                        ZStack {
+
+                    Button{
+                        isShowingModal = true
+                    } label: {
+                        ZStack{
                             Rectangle()
                                 .frame(width: 350, height: 60)
                                 .foregroundColor(Color("PrimaryGB"))
@@ -65,17 +69,17 @@ struct DirectItemCheckView: View {
                             }
                         }
                         .padding(.bottom, 30)
-//                        .onTapGesture {
-//                            registerItemsToCoreData()
-//                        }
-                        .simultaneousGesture(TapGesture().onEnded {
-                            registerItemsToCoreData()
-                        })
-                    })
+                    }
 
                 }
             }
+            .onDisappear(){
+                registerItemsToCoreData()
+            }
             .navigationBarBackButtonHidden(true)
+        }
+        .fullScreenCover(isPresented: $isShowingModal){
+            RegisterCompleteView(appState: appState)
         }
     }
     private var ListTitle: some View {
@@ -83,27 +87,27 @@ struct DirectItemCheckView: View {
             Text(viewModel.dateString)
                 .foregroundColor(Color("Gray900"))
                 .font(.system(size: 20.adjusted).weight(.bold))
-            
+
             Spacer()
-            
-            
+
+
         }
         .padding([.top, .bottom], 17.adjusted)
         .padding(.leading, 20.adjusted)
     }
-    
+
     private var ListContents: some View{
         ZStack{
             RoundedRectangle(cornerRadius: 15)
                 .foregroundColor(Color("Gray50"))
                 .padding(.horizontal, 20)
-            
+
             VStack{
-                
+
                 listDetail(listTraling: "품목", listLeading: "금액", listColor: "Gray400")
                     .padding(.horizontal, 40)
                     .padding(.top)
-                
+
                 Divider()
                     .overlay(Color("Gray100"))
 
@@ -118,25 +122,25 @@ struct DirectItemCheckView: View {
             }
         }
     }
-    
-    
+
+
     // TODO: listTraling에 품목을, listLeading에 금액을 넣어야 해요.
     private func listDetail(listTraling: String, listLeading: String, listColor: String) -> some View{
         return HStack{
             Text(listTraling)
                 .foregroundColor(Color(listColor))
                 .font(.system(size: 17.adjusted).weight(.semibold))
-            
+
             Spacer()
-            
+
             Text(listLeading)
                 .foregroundColor(Color(listColor))
                 .font(.system(size: 14.adjusted).weight(.semibold))
         }
         .padding([.top, .bottom], 8.adjusted)
     }
-    
-    
+
+
     func registerItemsToCoreData() {
         viewModel.itemBlockViewModels.forEach { item in
             coreDataViewModel.createReceiptData(name: item.name, price: Double(item.price))
