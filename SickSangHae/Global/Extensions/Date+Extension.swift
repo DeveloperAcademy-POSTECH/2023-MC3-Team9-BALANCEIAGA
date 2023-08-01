2e4f793f05baee4660ea091218cafc96fcc5c34e
ChangJin-Lee <ckdwls525@gmail.com>
2023-08-01 22:09:07 +0900

    chore: update Date+Extension formatter
    chore: update Date+Extension formatter

--- SickSangHae/Global/Extensions/Date+Extension.swift
+++ SickSangHae/Global/Extensions/Date+Extension.swift
@@ -24,4 +24,23 @@ extension Date {
         let dateDifference = Calendar.current.dateComponents([.day], from: self, to: dateTermFromNow).day
         return String(dateDifference ?? 90)
     }
+
+    var formattedMonth: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "M" // Format to display month and year
+        return formatter.string(from: self)
+    }
+
+    func extractMonthNumber(from dateString: String) -> String? {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy년 MM월 dd일"
+
+            if let date = formatter.date(from: dateString) {
+                let calendar = Calendar.current
+                let monthNumber = calendar.component(.month, from: date)
+                return String(monthNumber)
+            } else {
+                return nil
+            }
+        }
 }
