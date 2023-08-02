981ee1bf754c19dcac44bcd3437863322a6c80a6
Lets <ruvan123@naver.com>
2023-08-02 18:00:48 +0900

    fix: fix TopAlertView design

--- SickSangHae/View/Reusable/TopAlertView.swift
+++ SickSangHae/View/Reusable/TopAlertView.swift
@@ -15,11 +15,11 @@ struct TopAlertView: View {
     var body: some View {
       switch viewModel.changedStatus {
       case .shortTermUnEaten:
-        TopAlertBaseView(iconImage: "img_eat", message: "\(viewModel.name) 항목이 일반으로 이동됐어요", backgroundColor: .pointRLight, strokeColor: .pointRMiddle, viewModel: viewModel)
+          TopAlertBaseView(iconImage: "img_eat", message: "\"기본\" 보관으로 변경했어요", backgroundColor: .primaryGLight, strokeColor: .primaryGMiddle, viewModel: viewModel)
       case .shortTermPinned:
-        TopAlertBaseView(iconImage: "img_eat", message: "\(viewModel.name) 항목이 빨리 먹어야해요로 이동됐어요", backgroundColor: .pointRLight, strokeColor: .pointRMiddle, viewModel: viewModel)
+        TopAlertBaseView(iconImage: "img_eat", message: "\"빨리 먹어야 해요\" 보관으로 변경했어요", backgroundColor: .primaryGLight, strokeColor: .primaryGMiddle, viewModel: viewModel)
       case .longTermUnEaten:
-        TopAlertBaseView(iconImage: "img_eat", message: "\(viewModel.name) 항목이 장기 보관으로 이동됐어요", backgroundColor: .alertGreen, strokeColor: .primaryGMiddle, viewModel: viewModel)
+        TopAlertBaseView(iconImage: "img_eat", message: "\"천천히 먹어도 돼요\" 보관으로 변경했어요", backgroundColor: .primaryGLight, strokeColor: .primaryGMiddle, viewModel: viewModel)
       default:
           TopAlertBaseView(iconImage: "img_eat", message: "\(viewModel.name) 항목이 일반으로 이동됐어요", backgroundColor: .alertGreen, strokeColor: .primaryGMiddle, viewModel: viewModel)
       }
@@ -39,14 +39,15 @@ struct TopAlertBaseView: View {
       ZStack(alignment: .leading) {
         RoundedRectangle(cornerRadius: 41.adjusted)
           .stroke(strokeColor, lineWidth: 1)
-          .background(backgroundColor)
+          .background(Color.gray100)
           .background(.ultraThickMaterial)
           .frame(height: 68.adjusted)
           .clipShape(RoundedRectangle(cornerRadius: 41.adjusted))
           .opacity(0.8)
         HStack(spacing: 10.adjusted) {
-          Image(iconImage)
+            Image(systemName: "checkmark.circle.fill")
             .resizable()
+            .foregroundColor(.primaryGB)
             .scaledToFill()
             .frame(width: 44.adjusted, height: 44.adjusted)
           VStack(alignment: .leading, spacing: 4.adjusted) {
@@ -69,53 +70,6 @@ struct TopAlertBaseView: View {
     }
   }
 }
-
-//struct TopAlertBaseView: View {
-//  var iconImage: String
-//  var message: String
-//  var backgroundColor: Color
-//  var strokeColor: Color
-//  @GestureState private var dragOffset = CGSize.zero
-//  @ObservedObject var viewModel: TopAlertViewModel
-//
-//  var body: some View {
-//    if viewModel.isAlertVisible {
-//      ZStack(alignment: .leading) {
-//        RoundedRectangle(cornerRadius: 41.adjusted)
-//          .stroke(strokeColor, lineWidth: 1)
-//          .background(backgroundColor)
-//          .background(.ultraThickMaterial)
-//          .frame(height: 68.adjusted)
-//          .clipShape(RoundedRectangle(cornerRadius: 41.adjusted))
-//          .opacity(0.8)
-//        HStack(spacing: 10.adjusted) {
-//          Image(iconImage)
-//            .resizable()
-//            .scaledToFill()
-//            .frame(width: 44.adjusted, height: 44.adjusted)
-//          VStack(alignment: .leading, spacing: 4.adjusted) {
-//            Text(message)
-//              .font(.system(size: 14).bold())
-//              .foregroundColor(.black)
-//            Text("눌러서 실행 취소")
-//              .font(.system(size: 12))
-//              .foregroundColor(.gray)
-//          }
-//        }
-//        .padding(.leading, 14.adjusted)
-//      }
-//      .padding([.leading, .trailing], 20.adjusted)
-//      .offset(y: max(dragOffset.height, 0))
-//      .gesture (DragGesture()
-//        .updating($dragOffset, body: { (value, dragOffset, _) in
-//           dragOffset = value.translation
-//         })
-//          .onEnded(viewModel.onDragEnded)
-//
-//      )
-//    }
-//  }
-//}
 
 struct TopAlertView_Previews: PreviewProvider {
     static var previews: some View {
