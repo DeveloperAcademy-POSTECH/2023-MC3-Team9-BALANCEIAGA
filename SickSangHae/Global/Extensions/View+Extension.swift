06b32fac5bf8546ab34cf9f83ff861c2a5f26f79
SangwooKim <mollangcow@gmail.com>
2023-08-10 01:29:00 +0900

    feature: add PopGesture extension

--- SickSangHae/Global/Extensions/View+Extension.swift
+++ SickSangHae/Global/Extensions/View+Extension.swift
@@ -14,3 +14,14 @@ extension View {
       to: nil, from: nil, for: nil)
   }
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
