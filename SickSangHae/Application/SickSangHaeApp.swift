bc7ad8d6346a52b114d4b78b94a5d076670d3294
HyoWon Choi <eve652358@gmail.com>
2023-09-19 13:27:00 +0900

    feature: firebase setting

--- SickSangHae/Application/SickSangHaeApp.swift
+++ SickSangHae/Application/SickSangHaeApp.swift
@@ -6,22 +6,34 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    
+    return true
+  }
+}
 
 @main
 struct SickSangHaeApp: App {
-    @StateObject private var cameraViewModelShared = CameraViewModel()
-    var body: some Scene {
-        WindowGroup {
-        let appState = AppState()
-            TabBarView(appState: appState)
-                .environmentObject(appState)
-                .environmentObject(cameraViewModelShared)
-        }
+  @StateObject private var cameraViewModelShared = CameraViewModel()
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      let appState = AppState()
+      TabBarView(appState: appState)
+        .environmentObject(appState)
+        .environmentObject(cameraViewModelShared)
     }
+  }
 }
 
 struct ContentView: View {
-   var body: some View {
-       Text("Hello")
-   }
+  var body: some View {
+    Text("Hello")
+  }
 }
