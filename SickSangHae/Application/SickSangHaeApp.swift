b2a528d177d73e405e75df443df78645c4abf2ba
Lets <ruvan123@naver.com>
2024-03-03 19:23:43 +0900

    feature: Add AppDelegate class for Firebase config and fix GoogleService-Info

--- SickSangHae/Application/SickSangHaeApp.swift
+++ SickSangHae/Application/SickSangHaeApp.swift
@@ -5,11 +5,15 @@
 //  Created by 최효원 on 2023/07/06.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct SickSangHaeApp: App {
     @StateObject private var cameraViewModelShared = CameraViewModel()
+   
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
         let appState = AppState()
@@ -24,4 +28,16 @@ struct ContentView: View {
    var body: some View {
        Text("Hello")
    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        print("Sicksanghae is starting up. This is sent from App Delegate class.")
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
