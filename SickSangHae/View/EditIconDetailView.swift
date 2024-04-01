55ab8080e6df066daaf926641115f2f8ba2a1d8b
Reene <vernum.amor@gmail.com>
2024-04-02 03:02:50 +0900

    Fix: asset name
    Fix the incorrect asset name

--- SickSangHae/View/EditIconDetailView.swift
+++ SickSangHae/View/EditIconDetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct EditIconDetailView: View {
     let iconTitles = ["건강식품", "견과류","곡류", "과일류", "과자류", "냉동식품류", "달걀", "델리/초간단식", "떡류", "만두", "면류", "밀키트", "밥", "빵류", "소스류", "수산물", "시리얼", "식용유류", "아이스크림", "우유/유제품", "육류", "음료류", "장류", "조미료류", "주류", "즉석식품류", "잼류", "차", "초콜릿류", "치킨", "채소류", "커피", "콩류", "통조림류", "해조류", "햄/소시지류"]
     
-    let iconImages = ["supplements", "nuts", "grains", "fruits", "snacks", "frozenFoods", "eggs", "deliFoods", "ddeuk", "dumples", "noodles", "mealkits", "rices", "breads", "sauces", "seafoods", "cereals", "cookingOils", "iceCream", "dairies", "meats", "beverages", "koreanSauces", "seasonings", "liquors", "retortFoods", "jams", "teas", "chocolates", "friedChicken", "vegetables", "coffee", "beans", "cannedFoods", "seaweeds", "hamSausage"]
+    let iconImages = ["supplements", "nuts", "grains", "fruits", "snacks", "frozenFoods", "eggs", "deliFoods", "ddeuk", "dumples", "noodles", "mealkits", "rices", "breads", "sauces", "seafoods", "cereals", "cookingOils", "icecream", "dairies", "meats", "beverages", "koreansauces", "seasonings", "liquors", "retortFoods", "jams", "teas", "chocolates", "friedChicken", "vegetables", "coffee", "beans", "cannedFoods", "seaweeds", "hamSausage"]
     
     var iconDict: Dictionary<String, String> {
         Dictionary(uniqueKeysWithValues: zip(iconImages, iconTitles))
